59c21523a8ab803842ab3628b1729d6859d577cf
kennyckk <kenny6222003@hotmail.com>
2023-05-09 22:35:23 +0800

    add DogDataModule and unit test in notebook

--- data_trainer.py
+++ data_trainer.py
@@ -4,7 +4,7 @@ import torch
 import torch.nn as nn
 from torch.utils.data.dataloader import DataLoader
 
-from dataset import Kinetics
+from dataset import Kinetics,DogDataset
 import data_transform as T
 
 class Collator(object):
@@ -34,6 +34,104 @@ class Collator(object):
 			minibatch.append(torch.from_numpy(label))
 		
 		return minibatch
+
+class DogDataModule(pl.LightningDataModule):
+	def __init__(self,
+				 configs,
+				 train_ann_path,
+				 val_ann_path=None,
+				 test_ann_path=None,
+				 ):
+		super().__init__()
+		self.train_ann_path = train_ann_path
+		self.val_ann_path = val_ann_path
+		self.test_ann_path = test_ann_path
+		self.configs = configs
+
+	# function to get every data instance of the video from the annotation csv file
+	def get_dataset(self, annotation_path, transform, temporal_sample):
+		dataset = DogDataset(
+			self.configs,
+			annotation_path,
+			transform=transform,
+			temporal_sample=temporal_sample)
+
+		return dataset
+
+	def setup(self, stage):
+		color_jitter = 0.4
+		scale = None
+
+		if self.configs.data_statics == 'imagenet':
+			mean, std = (0.485, 0.456, 0.406), (0.229, 0.224, 0.225)
+		elif self.configs.data_statics == 'kinetics':
+			mean, std = (0.45, 0.45, 0.45), (0.225, 0.225, 0.225)
+		else:
+			mean, std = (0.5, 0.5, 0.5), (0.5, 0.5, 0.5)
+
+		# prepare the transformation needed for training data
+		train_transform = T.create_video_transform(
+			objective=self.configs.objective,
+			input_size=self.configs.img_size,
+			is_training=True,
+			scale=scale,
+			hflip=0.5,
+			color_jitter=color_jitter,
+			auto_augment=self.configs.auto_augment,
+			interpolation='bicubic',
+			mean=mean,
+			std=std)
+
+		#prepare sampling of frames from videos
+		train_temporal_sample = T.TemporalRandomCrop(
+		self.configs.num_frames * self.configs.frame_interval)
+
+		# call out the DogDataset to process the training data
+		self.train_dataset = self.get_dataset(
+			self.train_ann_path,
+			train_transform,
+			train_temporal_sample)
+		#prepare the transformation needed for eval data
+		assert (self.val_ann_path is not None), "please input valid ann eval path"
+
+		val_transform = T.create_video_transform(
+			input_size=self.configs.img_size,
+			is_training=False,
+			interpolation='bicubic',
+			mean=mean,
+			std=std)
+		# prepare sampling of frames from videos
+		val_temporal_sample = T.TemporalRandomCrop(
+			self.configs.num_frames * self.configs.frame_interval)
+		# call out the DogDataset to process the eval data
+		self.val_dataset = self.get_dataset(
+			self.val_ann_path,
+			val_transform,
+			val_temporal_sample)
+
+		# load in training dataloader
+	def train_dataloader(self):
+		return DataLoader(
+			self.train_dataset,
+			batch_size=self.configs.batch_size,
+			num_workers=self.configs.num_workers,
+			collate_fn=Collator(self.configs.objective).collate,
+			shuffle=True,
+			drop_last=True,
+			pin_memory=True
+		)
+
+	def val_dataloader(self):
+		return DataLoader(
+			self.val_dataset,
+			batch_size=self.configs.batch_size,
+			num_workers=self.configs.num_workers,
+			collate_fn=Collator(self.configs.objective).collate,
+			shuffle=False,
+			drop_last=False,
+			)
+
+
 
 class KineticsDataModule(pl.LightningDataModule):
 	def __init__(self, 
